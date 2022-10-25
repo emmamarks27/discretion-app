7c80adda24a8ff966f0fd35bdf30297fa7d7b5fe
emmamarks27 <emma_marks27@outlook.com			git config --global user.name emmamarks27>
2022-10-25 18:59:09 +0100

    Authentication for viewing posts added

--- api/routers/post.js
+++ api/routers/post.js
@@ -5,15 +5,14 @@ const postController = require('../controllers/post.js');
 const authenticator = require('../middleware/authenticator');
 const postRouter = Router();
 
-postRouter.get('/', postController.index);
-postRouter.post('/', postController.create);
-postRouter.get('/:id', postController.show);
-postRouter.delete('/:id', postController.destroy);
-//PUt the middleware on top of index - you can only see posts if you have a cookie - are logged in .
 postRouter.get('/', authenticator, postController.index);
 postRouter.post('/', postController.create);
 postRouter.get('/:id', postController.show);
 postRouter.delete('/:id', postController.destroy);
+//PUt the middleware on top of index - you can only see posts if you have a cookie - are logged in .
+// postRouter.get('/', authenticator, postController.index);
+// postRouter.post('/', postController.create);
+// postRouter.get('/:id', postController.show);
+// postRouter.delete('/:id', postController.destroy);
 
-module.exports = postRouter;
 module.exports = postRouter;
