dfdc0e620f2df164291dd2f9131e4d073c288ef0
emmamarks27 <emma_marks27@outlook.com			git config --global user.name emmamarks27>
2022-10-25 16:14:42 +0100

    Update user authentication

--- client/assets/board.js
+++ client/assets/board.js
@@ -1,70 +1,70 @@
-function createPostElement (data) {
-    const post = document.createElement("div");
-    post.className = "post";
+function createPostElement(data) {
+  const post = document.createElement('div');
+  post.className = 'post';
 
-    const header = document.createElement("h2");
-    header.textContent = data["title"];
-    post.appendChild(header);
+  const header = document.createElement('h2');
+  header.textContent = data['title'];
+  post.appendChild(header);
 
-    const content = document.createElement("p");
-    content.textContent = data["content"];
-    post.appendChild(content);
+  const content = document.createElement('p');
+  content.textContent = data['content'];
+  post.appendChild(content);
 
-    const sender = document.createElement("em");
-    sender.textContent = data["sender_name"];
-    post.appendChild(sender);
+  const sender = document.createElement('em');
+  sender.textContent = data['sender_name'];
+  post.appendChild(sender);
 
-    return post;
+  return post;
 }
 
-document.getElementById("post-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+document.getElementById('post-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-    const form = new FormData(e.target);
-    
-    const options = {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            title: form.get("title"),
-            content: form.get("content"),
-            sender_id: form.get("sender_id"),
-            recipient_id: form.get("recipient_id")
-        })
-    }
+  const form = new FormData(e.target);
 
-    const result = await fetch("http://localhost:3000/posts", options);
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: localStorage.getItem('discretionUser'),
+    },
+    body: JSON.stringify({
+      title: form.get('title'),
+      content: form.get('content'),
+      sender_id: form.get('sender_id'),
+      recipient_id: form.get('recipient_id'),
+    }),
+  };
 
-    if (result.status == 201) {
-        window.location.reload();
-    }
-})
+  const result = await fetch('http://localhost:3000/posts', options);
 
-async function loadPosts () {
+  if (result.status == 201) {
+    window.location.reload();
+  }
+});
 
-    const options = {
-        headers: {
-            'Authorization': localStorage.getItem("discretionUser")
-        }
-    }   
+async function loadPosts() {
+  const options = {
+    headers: {
+      Authorization: localStorage.getItem('discretionUser'),
+    },
+  };
 
-    const response = await fetch("http://localhost:3000/posts", options);
+  const response = await fetch('http://localhost:3000/posts', options);
 
-    if (response.status == 200) {
-        const posts = await response.json();
-    
-        const container = document.getElementById("posts");
+  if (response.status == 200) {
+    const posts = await response.json();
 
-        posts.forEach(p => {
-            const elem = createPostElement(p);
-            container.appendChild(elem);
-        })
-    } else {
-        window.location.assign("./index.html");
-    }
+    const container = document.getElementById('posts');
+
+    posts.forEach((p) => {
+      const elem = createPostElement(p);
+      container.appendChild(elem);
+    });
+  } else {
+    window.location.assign('./index.html');
+  }
 }
 
 loadPosts();
