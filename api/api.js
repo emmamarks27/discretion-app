e6a783a47d21d3696b5de50d97d691f5a04bb08f
emmamarks27 <emma_marks27@outlook.com			git config --global user.name emmamarks27>
2022-10-25 17:07:58 +0100

    resolving merge conflicts

--- api/api.js
+++ api/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+// const cookieParser = require("cookie-parser");
 
 const logRoutes = require('./middleware/logger');
 const postRouter = require('./routers/post');
@@ -9,16 +10,17 @@ const api = express();
 
 api.use(cors());
 api.use(express.json());
+// api.use(cookieParser());
 api.use(logRoutes);
 
-api.get("/", (req, res) => {
-    res.json({
-        name: "Discretion",
-        description: "Send and receive private messages."
-    })
-})
+api.get('/', (req, res) => {
+  res.json({
+    name: 'Discretion',
+    description: 'Send and receive private messages.',
+  });
+});
 
-api.use("/posts", postRouter);
-api.use("/users", userRouter);
+api.use('/posts', postRouter);
+api.use('/users', userRouter);
 
 module.exports = api;
